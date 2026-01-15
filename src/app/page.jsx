f46bb6f3311147873a83dcdec35d93ad4ecb230e
rashel9255 <ibrahim2000167@gmail.com>
2026-01-15 12:29:16 +0600

    feat: create navbar

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import divider from "daisyui/components/divider";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
